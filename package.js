00005668f09b5c3dfc182983693a3c0813b96c9e
Michael Lill <michael.lill@corefihub.com>
2021-01-29 10:50:02 +0100

    cleanup package.js, set version 0.5.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'corefihub:autoform-modals',
     summary: "Create, update and delete collections with modals",
-    version: "0.5.1",
+    version: "0.5.2",
     git: "https://github.com/corefihub/meteor-autoform-modals",
 });
 
@@ -9,13 +9,10 @@ Package.onUse(function (api) {
     api.versionsFrom('1.12.1');
 
     api.use([
-        // 'ecmascript',
         'blaze-html-templates@1.1.2',
         'less@3.0.1',
         'session',
         'coffeescript@2.4.1',
-        // 'underscore',
-        // 'ui',
         'aldeed:autoform@6.3.0',
         'raix:handlebar-helpers@0.2.5', // TODO remove this unmaintained package!!
         'mpowaga:string-template@0.1.0', // TODO remove this package!! makes use of "eval"!!!
